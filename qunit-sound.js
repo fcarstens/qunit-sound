d2a799be6e16d96656b29b2a22bc2815d3026ab4
Franziska Carstens <franziska.carstens@wwu.de>
2015-03-13 16:30:14 +0100

    fix url

--- qunit-sound.js
+++ qunit-sound.js
@@ -1,18 +1,17 @@
-
+// Play a sound once the tests are finished
 
 var success = new Howl({
-  urls: ['success.mp3']
+  urls: ['//fcarstens.github.io/qunit-sound/success.mp3']
   });
 
 var failed = new Howl({
-  urls: ['fail.mp3']
+  urls: ['//fcarstens.github.io/qunit-sound/fail.mp3']
 });
 
 QUnit.done(function( details ) {
-  if(details.failed > 0){
+  if(details.failed > 0) {
     failed.play();
-  }
-  else{
+  } else {
     success.play();
   }
 });
